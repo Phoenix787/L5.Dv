c5726d070228be8b3932096eacfe77ecf6aa912c
Phoenix787 <shiny777@bk.ru>
2016-03-23 12:00:02 +0500

    add menuItem

--- src/main/java/U2/L3/loginwindow/LoginController.java
+++ src/main/java/U2/L3/loginwindow/LoginController.java
@@ -4,6 +4,9 @@ package U2.L3.loginwindow;
  * Created by Сергеева on 23.03.2016.
  */
 public class LoginController {
+    //подключить модель финансового менеджера
+
+
     public static boolean authenticate(String name, String password) {
         /* выборка из базы данных */
         return name.equals("admin") && password.equals("admin");
