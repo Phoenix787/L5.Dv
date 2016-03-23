60de6ea2831c61c0aebcabd513bd4bc3cd30fdf7
Phoenix787 <shiny777@bk.ru>
2016-03-23 11:58:22 +0500

    add menuItem

--- src/main/java/U2/L3/loginwindow/LoginFrame.java
+++ src/main/java/U2/L3/loginwindow/LoginFrame.java
@@ -1,38 +1,46 @@
 package U2.L3.loginwindow;
 
 import javax.swing.*;
-import javax.swing.border.LineBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
 import javax.swing.plaf.FontUIResource;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 /**
  * Created by Сергеева on 23.03.2016.
  */
 public class LoginFrame extends JFrame {
 
-    JTextField tfUsername;
-    JPasswordField pfPassword;
-    JLabel lbRegister;
-    JLabel lbUsername;
-    JLabel lbPassword;
+    private static final long serialVersionUID = -321545275502211256L;
 
-    JButton btnLogin;
-    JButton btnCancel;
+    private JTextField tfUsername;
+    private JPasswordField pfPassword;
+    private JLabel lbRegister;
+    private JLabel lbUsername;
+    private JLabel lbPassword;
+
+    private JButton btnLogin;
+    private JButton btnCancel;
     private boolean succeeded;
 
 
     LoginFrame(){
 
+        setTitle("Login");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         JPanel panel = new JPanel(new GridBagLayout());
-        panel.setBorder(new LineBorder(Color.gray));
+        panel.setBorder(new CompoundBorder(new EmptyBorder(5,5,5,5), new EtchedBorder()));
         GridBagConstraints cs = new GridBagConstraints();
+        cs.insets = new Insets(2,2,2,2);
         cs.fill = GridBagConstraints.HORIZONTAL;
         lbUsername = new JLabel("Username: ");
         cs.gridx = 0;
         cs.gridy = 0;
+        cs.ipadx = 5;
+        cs.ipady = 5;
         cs.gridwidth = 1;
         panel.add(lbUsername, cs);
 
@@ -57,55 +65,48 @@ public class LoginFrame extends JFrame {
         lbRegister = new JLabel("Register", JLabel.RIGHT);
         lbRegister.setFont(new Font(Font.SANS_SERIF, FontUIResource.BOLD, 12));
         lbRegister.setForeground(Color.blue);
+        lbRegister.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // TODO: 23.03.2016 написать открытие формы Регистрация
+                System.out.println("Регистрация");
+            }
+        });
         cs.gridx = 1;
         cs.gridy = 2;
         cs.gridwidth = 2;
         panel.add(lbRegister, cs);
 
         btnLogin = new JButton("Login");
-        btnLogin.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (LoginController.authenticate(getUserName(), getPassword())){
-                    JOptionPane.showMessageDialog(LoginFrame.this, "Hi " + getUserName() + "!\nYou have " +
-                            "successfully logged in.", "Login", JOptionPane.INFORMATION_MESSAGE);
-                    succeeded = true;
-                    // TODO: 23.03.2016 сделать чтобы форма открывалась посередине рабочего стола
-                    // TODO: 23.03.2016  сделать открытие главного окна финансового менеджера
-                    dispose();
-                } else {
-                    JOptionPane.showMessageDialog(LoginFrame.this, "Invalid username or password", "Login",
-                            JOptionPane.ERROR_MESSAGE);
-                    tfUsername.setText("");
-                    pfPassword.setText("");
-                    succeeded = false;
-
-                }
+        btnLogin.addActionListener(e -> {
+            if (LoginController.authenticate(getUserName(), getPassword())){
+                JOptionPane.showMessageDialog(LoginFrame.this, "Hi, " + getUserName() + "!\nYou have " +
+                        "successfully logged in.", "Login", JOptionPane.INFORMATION_MESSAGE);
+                succeeded = true;
+                // TODO: 23.03.2016 сделать чтобы форма открывалась посередине рабочего стола
+                // TODO: 23.03.2016  сделать открытие главного окна финансового менеджера
+                dispose();
+            } else {
+                JOptionPane.showMessageDialog(LoginFrame.this, "Invalid username or password", "Login",
+                        JOptionPane.ERROR_MESSAGE);
+                tfUsername.setText("");
+                pfPassword.setText("");
+                succeeded = false;
 
             }
+
         });
         btnCancel = new JButton("Cancel");
-        btnCancel.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dispose();
-            }
-        });
+        btnCancel.addActionListener(e -> dispose());
 
         JPanel btnPanel = new JPanel();
         btnPanel.add(btnLogin);
         btnPanel.add(btnCancel);
 
-
         getContentPane().add(panel, BorderLayout.CENTER);
         getContentPane().add(btnPanel, BorderLayout.PAGE_END);
         setResizable(false);
         pack();
-
-
-
-
-
 
     }
 
@@ -115,6 +116,10 @@ public class LoginFrame extends JFrame {
 
     private String getUserName() {
         return tfUsername.getText();
+    }
+
+    public boolean isSucceeded() {
+        return succeeded;
     }
 
 }
