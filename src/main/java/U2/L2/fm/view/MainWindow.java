1dd3a255ce8d54222049a791f300dca19dbc8fa9
Phoenix787 <shiny777@bk.ru>
2016-03-24 18:29:47 +0500

    Merge branch 'master' of C:\Users\Ксения\Documents\L5.D-master with conflicts.

--- src/main/java/U2/L2/fm/view/MainWindow.java
+++ src/main/java/U2/L2/fm/view/MainWindow.java
@@ -1,44 +1,20 @@
 package U2.L2.fm.view;
 
 import U2.L2.fm.controller.GUI;
-import U2.L2.fm.model.datasets.Account;
 
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.Set;
 
 /**
  * Created by Ксения on 19.03.2016.
  */
 public class MainWindow extends JFrame {
-    private final GUI controller;
-    private DefaultListModel<String> dim;
 
     public MainWindow(GUI controller){
-        this.controller = controller;
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosed(WindowEvent e) {
-                System.out.println("Account close");
-                super.windowClosed(e);
-
-
-            }
-        });
-
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        Toolkit kit = Toolkit.getDefaultToolkit();
-        Dimension screenSize = kit.getScreenSize();
-        int screenWith = screenSize.width;
-        int screenHeight = screenSize.height;
-        setSize(screenWith / 2, screenHeight / 2);
-        setLocation(screenWith/2 - this.getWidth()/2, screenHeight/2 - this.getHeight() / 2);
-        //setLocationRelativeTo(null);
-
+        setSize(500, 300);
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.insets = new Insets(5, 5, 5, 5);
@@ -51,59 +27,23 @@ public class MainWindow extends JFrame {
         constraints.gridy = 0;
         panel.add(lbUser, constraints);
 
-//        Set<Account> temp = controller.getAccounts(controller.getOwner());
-//        Account[] data = temp.toArray(new Account[temp.size()]);
-//        String[] strData = new String[temp.size()];
-//        for (int i = 0; i < strData.length; i++) {
-//            strData[i] = data[i].getDescription();
-//        }
-
-        dim = new DefaultListModel<>();
-
-        String[] strData = controller.updateListAccount();
-        for (int i = 0; i < strData.length; i++) {
-            dim.add(i, strData[i]);
-        }
-        JList<String> listUsers = new JList<>(strData);
-        listUsers.setModel(dim);
+        Set<String> temp = controller.getNames();
+        String[] data = temp.toArray(new String[temp.size()]);
+        JList<String> listUsers = new JList<>(data);
         constraints.gridx = 0;
         constraints.gridy = 1;
         constraints.weighty = 2;
         panel.add(listUsers, constraints);
-
-        JPanel btnPanel = new JPanel();
-
-        JButton btnAddAccount = new JButton("Add account...");
-        btnAddAccount.addActionListener(e->{
-            SwingUtilities.invokeLater(()->{
-                new AccountForm(controller).start();
-
-            });
-        });
-
-        btnPanel.add(btnAddAccount);
 
 
 
 
 
         getContentPane().add(panel, BorderLayout.CENTER);
-        getContentPane().add(btnPanel, BorderLayout.PAGE_END);
     }
 
     public void start(){
-
         setVisible(true);
     }
-
-//    public String[] updateListAccount() {
-//        Set<Account> temp = controller.getAccounts(controller.getOwner());
-//        Account[] data = temp.toArray(new Account[temp.size()]);
-//        String[] strData = new String[temp.size()];
-//        for (int i = 0; i < strData.length; i++) {
-//            strData[i] = data[i].getDescription();
-//        }
-//        return strData;
-//    }
 
 }
